529a61ea15d86f8e0126e044b522dbd1f2d1606f
Haiko13 <haikobeater@gmail.com>
2023-01-24 10:54:41 +0800

    UAS_WebPrograming

---
+++ UAS_WebPrograming/resources/views/code.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.default')
+
+@section('content')
+<section>
+    <div class="conttainer mt-5">
+        <div class="row">
+            <div class="col-lg-8">
+<h3>Project Coding</h3>
+</div>
+          <div class ="col-lg-55">
+          <table class="table table-bordered border-primary">
+            <tr>
+            <center><img src="img/Coding Sound Sensor.png " width="1080px" ></center>
+
+            </tr>
+                    
+                    
+</section>
+@endsection
